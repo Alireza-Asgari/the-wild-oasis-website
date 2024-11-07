c8408c38834b7043cc4cfe8de4efe6d9ed597a7c
Alireza <alireza2023asgari@gmail.com>
2024-11-07 12:41:36 +0330

    add pages

---
+++ app/layout.js
@@ -0,0 +1,33 @@
+import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+export const metadata = {
+  // title: "Th Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites , surounded by beautifull forests ",
+};
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className}antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative `}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
+        <footer>
+          <p>copy right by wild oasis</p>
+        </footer>
+      </body>
+    </html>
+  );
+}
