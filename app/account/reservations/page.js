04af6360d17e3a5826135a3f7c4c209e9e37388d
Alireza <alireza2023asgari@gmail.com>
2024-11-13 12:11:18 +0330

    Building guest reservation page

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,11 +1,13 @@
 import ReservationCard from "@/app/_components/ReservationCard";
+import { auth } from "@/app/_lib/auth";
+import { getBookings } from "@/app/_lib/data-service";
 export const metadata = {
   title: "Reservation",
 };
-export default function Page() {
-  // CHANGE
-  const bookings = [];
-
+export default async function Page() {
+  const session = await auth();
+  const bookings = await getBookings(session.user.guestId);
+  console.log(bookings);
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
